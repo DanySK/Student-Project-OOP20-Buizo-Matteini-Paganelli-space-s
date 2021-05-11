b7ef5820087a0e15dd91d1be9a0bf86e35eb36fe
Alberto Paganelli <albi1600@gmail.com>
2021-05-11 16:05:34 +0200

    collisioni rectboundingbox dobvrebbero essere ok

--- src/model/worldEcollisioni/physics/RectBoundingBox.java
+++ src/model/worldEcollisioni/physics/RectBoundingBox.java
@@ -21,53 +21,45 @@ public class RectBoundingBox implements BoundingBox {
 	}
 	
 	public double getWidth(){
-		return this.p0.getX() - this.p1.getX();
+		return this.p1.getX() - this.p0.getX();
 	}
 	
 	public double getHeight(){
-		return this.p0.getY() - this.p1.getY();
+		return this.p1.getY() - this.p0.getY();
 	}
 	
 	/**
 	 * @TODO to be implemented
-	 * Il raggio sar‡ il lato.
+	 * Il raggio sar√† il lato.
 	 */
 	public boolean isCollidingWith(P2d p, double radius){
 		
 		
-		
-		System.out.println(p.toString());
-			//boolean returnn = 
-					//new V2d(p,radius).module() <= radius+this.radius;
+		if(p.getX() <= this.getULCorner().getX() + this.getWidth() &&
+				p.getY() <= this.getULCorner().getY() + this.getHeight()) {
+			return true;
+		}
 			
-			
-		
-		
-		
-//		if (ball.getX() + ball.getR() >= rect.getTheLeft() &&
+//		if (this.getULCorner().getX() + this.getWidth() >= p.getX() &&
 //			    ball.getX() - ball.getR() <= rect.getTheRight() &&
 //			    ball.getY() - ball.getR() <= rect.getTheBottom() &&
 //			    ball.getY() + ball.getR() >= rect.getTheTop())
 //			{
 //			    intersects = true;
 //			}
-		
-		//System.out.println("check the collision");
-		return false;
+//		
+
+			return false;
 	}
 	
 	public static void main (String[] args) {
 		System.out.println("Ciao collisioni");
 		
 		RectBoundingBox r1 = new RectBoundingBox(new P2d(0,0), new P2d(10,10));
-		RectBoundingBox r2 = new RectBoundingBox(new P2d(5,5), new P2d(15,15));
+		RectBoundingBox r2 = new RectBoundingBox(new P2d(7,7), new P2d(15,15));
 		
-		System.out.println(r1.toString());
-		System.out.println(r2.toString());
-		r1.isCollidingWith(r1.getULCorner(), 10);
-
 		
-		System.out.println(r1.isCollidingWith(r1.getULCorner(), 10));
+		System.out.println(r1.isCollidingWith(r2.getULCorner(), 10));
 		
 	}
 }
