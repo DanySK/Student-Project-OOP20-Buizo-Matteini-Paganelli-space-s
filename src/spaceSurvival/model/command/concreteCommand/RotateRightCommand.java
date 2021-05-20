7dd688b44a306fbeb8d150e7eb26b5a9dd1c7d2c
Manuel <buizomanuel@gmail.com>
2021-05-20 19:15:52 +0200

    eureka movement go

--- src/spaceSurvival/model/command/concreteCommand/RotateRightCommand.java
+++ src/spaceSurvival/model/command/concreteCommand/RotateRightCommand.java
@@ -15,54 +15,24 @@ public class RotateRightCommand implements CommandMovement{
 	@Override
 	public void execute(MovableGameObject obj) {
 		System.out.println("Rotate Right");
-		
-//		System.out.println(obj);
-//		
-//		SpaceShipSingleton ship = (SpaceShipSingleton) obj;
-//		AffineTransform transform = ship.getTransform();
-//		//transform.rotate(Math.toRadians(15));
-//		//transform.rotate(Math.toRadians(15), ship.getSize().getHeight() / 2, ship.getSize().getWidth() / 2);
-//		
-//				//, ship.getSize().getHeight() / 2, ship.getSize().getWidth() / 2);
-//		transform.rotate(Math.toRadians(15));
-//		System.out.println(ship.getTransform());
-//		//transform.rotate
-//		//transform.rotate(Math.toRadians(-15));
-//		ship.setTransform(transform);
-//		
-//		System.out.println(ship.toString());
-		
-		
-//		SpaceShipSingleton ship = (SpaceShipSingleton) obj;
-//		AffineTransform transform = ship.getTransform();
-//
-//		RectBoundingBox bbox = (RectBoundingBox) ship.getBoundingBox();
-//		
-//		
-//		double xCenter = (ship.getTransform().getTranslateX() ) + ship.getSize().getWidth() / 2;
-//		double yCenter = (ship.getTransform().getTranslateY() ) + ship.getSize().getHeight() / 2;	
-//
-//		
-//		System.out.println("xCenter" + ship.getSize().getWidth() / 2);
-//		System.out.println("yCenter" + ship.getSize().getHeight() / 2);
-//		
-//		System.out.println("ScaleX" + transform.getScaleX());
-//		
-//		
-//		transform.rotate(Math.toRadians(15), ship.getSize().getWidth() / 2, 50);
-//
-//		ship.setTransform(transform); 
+
 		SpaceShipSingleton ship = (SpaceShipSingleton) obj;
 		AffineTransform transform = ship.getTransform();
+		//RectBoundingBox bbox = (RectBoundingBox) ship.getBoundingBox();
+		//AffineTransform transform2 = bbox.getTransform();
 
-		RectBoundingBox bbox = (RectBoundingBox) ship.getBoundingBox();
-		
-		
+
 		//double xCenter = (ship.getTransform().getTranslateX() ) + ship.getSize().getWidth() / 2;
-		//double yCenter = (ship.getTransform().getTranslateY() ) + ship.getSize().getHeight() / 2;	
+		//double yCenter = (ship.getTransform().getTranslateY() ) + ship.getSize().getHeight() / 2;
 
 		transform.rotate(Math.toRadians(15), ship.getSize().getWidth() / 2, ship.getSize().getHeight() / 2);
+		//transform2.rotate(Math.toRadians(-15), ship.getSize().getWidth() / 2, ship.getSize().getHeight() / 2);
 
+		//bbox.getTransform().rotate(Math.toRadians(-15), Screen.POINT_CENTER_FULLSCREEN.getX(), Screen.POINT_CENTER_FULLSCREEN.getY());
+
+		//RectBoundingBox bbox = (RectBoundingBox) ship.getBoundingBox();
+		//transform.rotate(Math.toRadians(-15), bbox.getWidth(), bbox.getHeight());
+		//System.out.println("Rotate Left" + bbox.toString());
 		ship.setTransform(transform);
 	}
 }
--- src/spaceSurvival/view/GUI/game/utilities/PanelGame.java
+++ src/spaceSurvival/view/GUI/game/utilities/PanelGame.java
@@ -29,41 +29,15 @@ public class PanelGame extends JPanel {
         final Graphics2D g2d = (Graphics2D) g;
 
         this.gameObject.forEach((key, value) -> {
-            g2d.drawImage(this.getImageFromPath(key.getEngineImage()), value, null);
-            BoundingBox bb = key.getBoundingBox();
-
-            //g2d.drawImage(this.getImageFromPath(new EngineImage(GameObjectUtils.SPACESHIP_SCALEOF, Screen.WIDTH_FULL_SCREEN, Skin.ATOMIC)), bb.getTransform(), null);
-           // g2d.drawRect((int)bb.getTransform().getTranslateX(), (int)bb.getTransform().getTranslateY(), (int) key.getSize().getWidth(),  (int) key.getSize().getHeight());
-            //RectBoundingBox rbb =  (RectBoundingBox) key.getBoundingBox();
-            
-            //double m00 = value.getScaleX();
-            //double m01 = value.getShearX();
-            //double angle = Math.atan2(-m01, m00);
-            
-            
-            //AffineTransform newAff = new AffineTransform();
-            //newAff.setToTranslation(value.getTranslateX(), value.getTranslateY());
-            
-            //rbb.getTransform().rotate(angle, key.getPosition().getX() + key.getEngineImage().getWidth()/2,  key.getPosition().getY()+ key.getEngineImage().getHeight()/2);
-
-            //newAff.translate(0, key.getSize().getHeight() * 2);
-            
-            //), gameObject.getSize().getWidth()/2, gameObject.getSize().getHeight()/2);
-            
-            //
-            //g2d.setColor(Color.GREEN);
-
-            //g2d.setTransform(rbb.getTransform());
-            //g2d.drawRect((int)value.getTranslateX(), (int)value.getTranslateY(), (int)rbb.getWidth(), (int)rbb.getHeight());
 
 
-           // g2d.drawRect((int)value.getTranslateX(), (int)value.getTranslateY(), (int) key.getEngineImage().getWidth(), (int) key.getEngineImage().getHeight());
-            //System.out.println("Width drawing: - >" + key.getEngineImage().getWidth());
-            //System.out.println("Height drawing: - >" + key.getEngineImage().getHeight());
-            
-            
+            g2d.setTransform(value);
+            g2d.drawImage(this.getImageFromPath(key.getEngineImage()), null, null);
+
+
             this.drawLifeBar(g2d, (MainGameObject) key, value);
-            //this.drawLife(g2d, (MainGameObject) key, value);
+            this.drawLife(g2d, (MainGameObject) key, value);
+
         });
     }
 
@@ -84,32 +58,18 @@ public class PanelGame extends JPanel {
 
 
     private void drawLifeBar(final Graphics2D g2d, final GameObject gameObject, final AffineTransform transform){
-      
-    	//final BoundingBox rect = gameObject.getBoundingBox();
-        //double m00 = transform.getScaleX();
-       // double m01 = transform.getShearX();
 
-        //final int x = (int) (transform.getTranslateX());
-        //final int y = (int) (transform.getTranslateY());
-    
-        //double angle = Math.atan2(-m01, m00);
-        
         AffineTransform aff = new AffineTransform();
         aff.setTransform(transform);
 
-        //aff.setToTranslation(transform.getTranslateX(), transform.getTranslateY());
-        //newAff.rotate(angle, gameObject.getPosition().getX(), gameObject.getPosition().getY());
-        //aff.setToTranslation(aff.getTranslateX(), aff.getTranslateY() + gameObject.getSize().getHeight());
         aff.translate(0, gameObject.getSize().getHeight());
-    System.out.println("TranslateY -> "+aff.getTranslateY()+"TranslateX -> "+aff.getTranslateX()+"objectSizeHeight ->" +  gameObject.getSize().getHeight());
 
-        //g2d.setTransform(aff);
-        g2d.drawImage(this.getImageFromPath(new EngineImage(GameObjectUtils.SPACESHIP_SCALEOF, Screen.WIDTH_FULL_SCREEN, Skin.ATOMIC)), aff, null);
+        g2d.setColor(Color.WHITE);
+        g2d.setTransform(aff);
+        RectBoundingBox bb = (RectBoundingBox) gameObject.getBoundingBox();
 
-        //g2d.setColor(Color.WHITE);
+        g2d.drawRect((int) 0,0, (int)gameObject.getSize().getHeight(), 11);
 
-        //g2d.fillRect(0,(int)gameObject.getEngineImage().getHeight(), (int)gameObject.getSize().getWidth(), 11);
-        //g2d.setTransform(new AffineTransform());
     }
 
 
@@ -117,26 +77,36 @@ public class PanelGame extends JPanel {
 
         //final RectBoundingBox rect = (RectBoundingBox) gameObject.getBoundingBox();
         
-        double m00 = transform.getScaleX();
-        double m01 = transform.getShearX();
+//        double m00 = transform.getScaleX();
+//        double m01 = transform.getShearX();
+//
+//        final int x = (int) (transform.getTranslateX());
+//        final int y = (int) (transform.getTranslateY());
+//
+//
+//        double angle = Math.atan2(-m01, m00);
+//
+//        AffineTransform newAff = new AffineTransform();
+//
+//        newAff.setToTranslation(transform.getTranslateX(), transform.getTranslateY());
+//        newAff.rotate(angle, gameObject.getPosition().getX(), gameObject.getPosition().getY());
+//        newAff.translate(0, gameObject.getEngineImage().getHeight());
+//        System.out.println("aaaaaaaaaaaaaaaaa" + gameObject.getEngineImage().getHeight());
+//
+//
+//        g2d.setTransform(newAff);
+//        g2d.setColor(Color.GREEN);
+//
+//        g2d.fillRect(x, y, 50, 11);
 
-        final int x = (int) (transform.getTranslateX());
-        final int y = (int) (transform.getTranslateY());
+        AffineTransform aff = new AffineTransform();
+        aff.setTransform(transform);
 
-         
-        double angle = Math.atan2(-m01, m00);
-        
-        AffineTransform newAff = new AffineTransform();
+        aff.translate(0, gameObject.getSize().getHeight());
 
-        newAff.setToTranslation(transform.getTranslateX(), transform.getTranslateY());      
-        newAff.rotate(angle, gameObject.getPosition().getX(), gameObject.getPosition().getY());
-        newAff.translate(0, gameObject.getEngineImage().getHeight());
-        System.out.println("aaaaaaaaaaaaaaaaa" + gameObject.getEngineImage().getHeight());
-
-
-        g2d.setTransform(newAff);
         g2d.setColor(Color.GREEN);
+        g2d.setTransform(aff);
 
-        g2d.fillRect(x, y, 50, 11);
+        g2d.fillRect((int) 0,1, 50, 10);
     }
 }
