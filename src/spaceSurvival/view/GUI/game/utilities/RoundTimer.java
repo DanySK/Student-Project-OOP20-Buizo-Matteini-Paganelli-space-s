62f20c4895ace4f9156f2988ee8ca69466623ce9
Bmanuel <buizomanuel@gmail.com>
2021-05-20 18:23:41 +0200

    solved position roundtimer

--- src/spaceSurvival/view/GUI/game/utilities/RoundTimer.java
+++ src/spaceSurvival/view/GUI/game/utilities/RoundTimer.java
@@ -21,23 +21,27 @@ public class RoundTimer extends JPanel {
 
         super.add(FactoryGUIs.encapsulatesInPanelFlow(this.timer));
         super.add(FactoryGUIs.createPanelFlowUnionComponents(java.util.List.of(this.stringRound, this.round)));
-
-//        super.setLayout(new GridLayout(0, super.getComponentCount() - 1));
+        super.setLayout(new GridLayout(0, super.getComponentCount() - 1));
     }
 
     public void setFontAll(final Font font){
         this.timer.setFont(font);
         this.stringRound.setFont(font);
         this.round.setFont(font);
-//        this.timer.setFont(DesignGraphics.getFontForGame(DesignGraphics.SIZE_FONT_H2));
-//        this.stringRound.setFont(DesignGraphics.getFontForGame(DesignGraphics.SIZE_FONT_H2));
-//        this.round.setFont(DesignGraphics.getFontForGame(DesignGraphics.SIZE_FONT_H2));
     }
 
     public void setForegroundAll(final Color color){
         this.timer.setForeground(color);
         this.stringRound.setForeground(color);
         this.round.setForeground(color);
+    }
+
+    public void setRound(final int round){
+        this.round.setText(Integer.toString(round));
+    }
+
+    public void setRound(final String round){
+        this.round.setText(round);
     }
 
     public void setTimer(final String timer){
