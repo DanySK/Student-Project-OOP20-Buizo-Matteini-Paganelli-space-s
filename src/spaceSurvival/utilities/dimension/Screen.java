c097a523f9786f48b9930b2beaf0564d4d0d1019
Bmanuel <buizomanuel@gmail.com>
2021-05-23 18:31:14 +0200

    merge paga

--- src/spaceSurvival/utilities/dimension/Screen.java
+++ src/spaceSurvival/utilities/dimension/Screen.java
@@ -15,24 +15,27 @@ public class Screen {
     public static final Dimension FULLSCREEN = SystemVariables.SCREEN;
     public static final int WIDTH_FULL_SCREEN = (int) (FULLSCREEN.getWidth());
     public static final int HEIGHT_FULL_SCREEN = (int) (FULLSCREEN.getHeight());
-    public static final P2d POINT_CENTER_FULLSCREEN = new P2d((WIDTH_FULL_SCREEN / 2) * SystemVariables.SCALE_X, (HEIGHT_FULL_SCREEN / 2) * SystemVariables.SCALE_Y);
-    public static final Rectangle RECTANGLE_FULLSCREEN = new Rectangle(POINT_ZERO, new Dimension(WIDTH_FULL_SCREEN, HEIGHT_FULL_SCREEN));
+
+    public static final P2d POINT_CENTER_FULLSCREEN = new P2d((WIDTH_FULL_SCREEN * SystemVariables.SCALE_X / 2),
+            (HEIGHT_FULL_SCREEN * SystemVariables.SCALE_Y / 2 ));
+    public static final Rectangle RECTANGLE_FULLSCREEN = new Rectangle(POINT_ZERO,
+            new Dimension(WIDTH_FULL_SCREEN, HEIGHT_FULL_SCREEN));
 
 
     public static final int WIDTH_BIG = (int) (WIDTH_FULL_SCREEN / PROPORTION_BIG);
     public static final int HEIGHT_BIG = (int) (HEIGHT_FULL_SCREEN / PROPORTION_BIG);
-    public static final int CENTER_X_BIG = (int) (POINT_CENTER_FULLSCREEN.getX() - (WIDTH_BIG / 2));
-    public static final int CENTER_Y_BIG = (int) (POINT_CENTER_FULLSCREEN.getY() - (HEIGHT_BIG / 2));
-
+    public static final int CENTER_X_BIG = (int) (POINT_CENTER_FULLSCREEN.getX() - (WIDTH_BIG * SystemVariables.SCALE_X / 2));
+    public static final int CENTER_Y_BIG = (int) (POINT_CENTER_FULLSCREEN.getY() - (HEIGHT_BIG * SystemVariables.SCALE_Y / 2));
     public static final Dimension DIMENSION_BIG = new Dimension(WIDTH_BIG, HEIGHT_BIG);
+
     public static final Point POINT_CENTER_BIG = new Point(CENTER_X_BIG, CENTER_Y_BIG);
     public static final Rectangle RECTANGLE_BIG = new Rectangle(POINT_CENTER_BIG, DIMENSION_BIG);
 
 
     public static final int WIDTH_MEDIUM = (int) (WIDTH_FULL_SCREEN/ PROPORTION_MEDIUM);
     public static final int HEIGHT_MEDIUM = (int) (HEIGHT_FULL_SCREEN / PROPORTION_MEDIUM);
-    public static final int CENTER_X_MEDIUM = (int) (POINT_CENTER_FULLSCREEN.getX() - (WIDTH_MEDIUM / 2));
-    public static final int CENTER_Y_MEDIUM = (int) (POINT_CENTER_FULLSCREEN.getY() - (HEIGHT_MEDIUM / 2));
+    public static final int CENTER_X_MEDIUM = (int) (POINT_CENTER_FULLSCREEN.getX() - (WIDTH_MEDIUM * SystemVariables.SCALE_X / 2));
+    public static final int CENTER_Y_MEDIUM = (int) (POINT_CENTER_FULLSCREEN.getY() - (HEIGHT_MEDIUM * SystemVariables.SCALE_Y / 2));
 
     public static final Dimension DIMENSION_MEDIUM = new Dimension(WIDTH_MEDIUM, HEIGHT_MEDIUM);
     public static final Point POINT_CENTER_MEDIUM = new Point(CENTER_X_MEDIUM, CENTER_Y_MEDIUM);
@@ -41,8 +44,8 @@ public class Screen {
 
     public static final int WIDTH_MINI = (int) (WIDTH_FULL_SCREEN / PROPORTION_MINI);
     public static final int HEIGHT_MINI = (int) (HEIGHT_FULL_SCREEN / PROPORTION_MINI);
-    public static final int CENTER_X_MINI = (int) (POINT_CENTER_FULLSCREEN.getX() - (WIDTH_MINI / 2));
-    public static final int CENTER_Y_MINI = (int) (POINT_CENTER_FULLSCREEN.getY() - (HEIGHT_MINI / 2));
+    public static final int CENTER_X_MINI = (int) (POINT_CENTER_FULLSCREEN.getX() - (WIDTH_MINI * SystemVariables.SCALE_X / 2));
+    public static final int CENTER_Y_MINI = (int) (POINT_CENTER_FULLSCREEN.getY() - (HEIGHT_MINI * SystemVariables.SCALE_X / 2));
 
     public static final Dimension DIMENSION_MINI = new Dimension(WIDTH_MINI, HEIGHT_MINI);
     public static final Point POINT_CENTER_MINI = new Point(CENTER_X_MINI, CENTER_Y_MINI);
