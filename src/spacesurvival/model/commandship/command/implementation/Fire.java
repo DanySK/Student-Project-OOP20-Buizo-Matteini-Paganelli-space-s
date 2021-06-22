25b5b88412287d69b6abd15a0ca3671b14f7e519
Mattia Matteini <matteinimattia@gmail.com>
2021-06-22 10:57:26 +0200

    added delay to ship fire

--- src/spacesurvival/model/commandship/command/implementation/Fire.java
+++ src/spacesurvival/model/commandship/command/implementation/Fire.java
@@ -2,17 +2,26 @@ package spacesurvival.model.commandship.command.implementation;
 
 import spacesurvival.model.commandship.command.CommandShip;
 import spacesurvival.model.gameobject.main.SpaceShipSingleton;
+import spacesurvival.utilities.Delay;
+import spacesurvival.utilities.ThreadUtils;
 import spacesurvival.utilities.path.SoundPath;
 
 public class Fire implements CommandShip {
-    /** 
+    /**
      * Fires the shot from the ship's weapon if it is present.
      * 
      * @param ship the controlled ship
      */
     @Override
     public void execute(final SpaceShipSingleton ship) {
-        ship.fire();
-        ship.getSoundQueue().add(SoundPath.SHOOT);
+        if (ship.getWeapon().canShoot()) {
+            ship.getWeapon().canShoot(false);
+            new Thread(() -> {
+                ThreadUtils.sleep(Delay.BULLET_SHOT);
+                ship.getWeapon().canShoot(true);
+            }).start();
+            ship.fire();
+            ship.getSoundQueue().add(SoundPath.SHOOT);
+        }
     }
 }
